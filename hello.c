f3e634c55353f87d621f15a5f062ca865b1939c3
Abdul-rasheed <rasheedw622@gmail.com>
2022-09-21 13:05:25 +0100

    rough works

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    
+    bool value = true;
+    bool valueb = false;
+
+    printf("%d ", value);
+    printf(" %d ", valueb);
+
+   
+    return 0;
+}
